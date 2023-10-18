2961f719799a38484cc792d3acb7482bb13a7bdf
alannarisse <alannarisse@users.noreply.github.com>
2023-10-18 08:52:45 -0700

    added new stuff

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-// your script file for adding your own jquery
-$(function() {
-// Your Code from here on down. Don't delete that line above!
-  
 
 $('.clickme').click(function(){
 		$('audio#owl')[0].play()
@@ -17,14 +13,8 @@ $('.restartme').click(function(){
 	$('audio#owl')[0].play()
 })
 
-
-
-
-
-
-
-
-// End of Your Code . Don't delete that line below!!
-});
+$('.text-btn').click(function(){
+	$('.text').toggleClass('hide')
+})
 
 
